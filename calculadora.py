a77e0ef088499bae6fea5ad8d132ae6eb9cfd5d1
Luis Manuel Vera <luismanuelvera50@gmail.com>
2023-05-19 08:07:20 -0400

    cambios en la señalizacion del menu

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,5 @@
 def menu():
-    print("     ...:MENU:...","\n","1-Sumar uno o mas numeros","\n","2-Restar uno o mas numeros","\n","3-Multiplicar uno o mas numeros\n","4-Dividir un numero\n","5-Salir del programa")
+    print("     =>MENU<=","\n","1-Sumar uno o mas numeros","\n","2-Restar uno o mas numeros","\n","3-Multiplicar uno o mas numeros\n","4-Dividir un numero\n","5-Salir del programa")
 
 exit = 0
 while exit == 0:
