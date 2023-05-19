15d0574ae490e419aa0f6dafc62312e166f7b284
Luis Manuel Vera <luismanuelvera50@gmail.com>
2023-05-19 12:30:29 -0400

    agregue la salida del programa

--- calculadora.py
+++ calculadora.py
@@ -2,10 +2,13 @@ def menu():
     print("     =>MENU<=","\n","1-Sumar uno o mas numeros","\n","2-Restar uno o mas numeros","\n","3-Multiplicar uno o mas numeros\n","4-Dividir un numero\n","5-Salir del programa")
 
 exit = 0
-while exit == 0:
+while exit != 1:
     menu()
     try:
         opcion = int(input("Eliga el valor correspondiente a la opcion deseada=>"))
     except ValueError:
         print("Valor incorrecto")
-    
+    try:   
+        exit = int(input("Desea salir del programa?: \n 1-Si \n 2-No"))
+    except ValueError:
+        print("Valor incorrecto")
