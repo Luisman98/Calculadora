b5649f6633ea6c33fb1ca0e91365e81fbb8de7cb
Luisman <luismanuelvera50@gamil.com>
2023-05-22 11:41:58 -0400

    hola

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,6 @@
 def menu():
     print("     =>MENU<=","\n","1-Sumar uno o mas numeros","\n","2-Restar uno o mas numeros","\n","3-Multiplicar uno o mas numeros\n","4-Dividir un numero\n","5-Salir del programa")
+#hola
 #comienza el programa
 while exit != 1:
     menu()
@@ -13,7 +14,7 @@ while exit != 1:
     #si la opcion es 5, sale del programa
     if opcion == 5:
         break
-    
+
     #aqui finaliza en programa y pregunta si desea repetir el programa
     try:
         exit = int(input("Desea salir del programa?: \n 1-Si \n 2-No \n=>"))
