d9cfe07d99f0cdaddb76f67f38a13aed19c24a18
Luisman <luismanuelvera50@gamil.com>
2023-05-22 11:37:34 -0400

    avance un poco en el proyecto

--- calculadora.py
+++ calculadora.py
@@ -1,13 +1,21 @@
 def menu():
     print("     =>MENU<=","\n","1-Sumar uno o mas numeros","\n","2-Restar uno o mas numeros","\n","3-Multiplicar uno o mas numeros\n","4-Dividir un numero\n","5-Salir del programa")
-
-exit = while exit != 1:
+#comienza el programa
+while exit != 1:
     menu()
+
+    #el usuario ingresa la opcion que desea
     try:
         opcion = int(input("Eliga el valor correspondiente a la opcion deseada=>"))
     except ValueError:
         print("Valor incorrecto")
-    try:   
-        exit = int(input("Desea salir del programa?: \n 1-Si \n 2-No"))
+    
+    #si la opcion es 5, sale del programa
+    if opcion == 5:
+        break
+    
+    #aqui finaliza en programa y pregunta si desea repetir el programa
+    try:
+        exit = int(input("Desea salir del programa?: \n 1-Si \n 2-No \n=>"))
     except ValueError:
         print("Valor incorrecto")
