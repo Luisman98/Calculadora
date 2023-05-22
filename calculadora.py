3dcc7fed6bceca7b6fd22050816cdeea22e022f2
Luisman <luismanuelvera50@gamil.com>
2023-05-22 11:07:07 -0400

    un cambio randm

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,7 @@
 def menu():
     print("     =>MENU<=","\n","1-Sumar uno o mas numeros","\n","2-Restar uno o mas numeros","\n","3-Multiplicar uno o mas numeros\n","4-Dividir un numero\n","5-Salir del programa")
 
-exit = 0
-while exit != 1:
+exit = while exit != 1:
     menu()
     try:
         opcion = int(input("Eliga el valor correspondiente a la opcion deseada=>"))
