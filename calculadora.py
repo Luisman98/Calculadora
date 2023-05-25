91543f7a807c020c761066dd3d7a637234b9495b
Luisman <luismanuelvera50@gamil.com>
2023-05-25 15:37:30 -0400

    implemetacion de la resta y arreglos en la suma

--- calculadora.py
+++ calculadora.py
@@ -10,26 +10,61 @@ while exit != 1:
     except ValueError:
         print("Valor incorrecto")
         continue
-    #Si la opcion es 1
+
+    #Si la opcion es 1 entra en la suma
     if opcion == 1:
         salir_suma = int
         while salir_suma != 1:
-            try:
+            try:        
                 num1_suma = int(input("Ingrese un numero:=>"))
-                num2_suma = int(input("Ingrese un numero a sumar:=>"))
+                num2_suma = int(input("Ingrese la cantidad que desea sumar:=>"))
                 result_suma = num1_suma + num2_suma
-                print(result_suma)
+                print("RESULTADO=>",result_suma)
+                opcion_user = 0
+                while opcion_user != 2:
+                    try:
+                        opcion_user = int(input("Desea agregar otro numero para suamrlo?: \n 1-Si \n 2-No \n =>"))
+                        if opcion_user == 1:
+                            num3_suma = int(input("Ingrese un numero a sumar:=>"))
+                            result_suma = result_suma + num3_suma
+                            print("RESULTADO=>",result_suma)
+                        salir_suma = int(input("Desea salir de la suma?: \n 1-Si \n 2-No \n =>"))
+                    except ValueError:
+                        print("Valor incorrecto")
+                        opcion_user = 0
+                        continue
             except ValueError:
                 print("Valor incorrecto")
+                continue
+
+    #Si la opcion es 2 entra en la resta
+    if opcion == 2:
+        salir_resta = int
+        while salir_resta != 1:
             try:
-                salir_suma = input("Desea salir de la suma?: \n 1-Si \n 2-No \n =>")
+                num1_resta = int(input("Ingrese un numero:=>"))
+                num2_resta = int(input("Ingrese la cantidad que desea restar:=>"))
+                result_resta = num1_resta - num2_resta
+                print("RESULTADO=>",result_resta)
+                opcion_user = 0
+                while opcion_user != 2:
+                    try:
+                        opcion_user = int(input("Desea agregar otro numero para restarlo?: \n 1-Si \n 2-No \n =>"))
+                        if opcion_user == 1:
+                            num3_resta = int(input("Ingrese un numero a restar:=>"))
+                            result_resta = result_resta - num3_resta
+                            print("RESULTADO=>",result_resta)
+                        salir_resta = int(input("Desea salir de la resta?: \n 1-Si \n 2-No \n =>"))
+                    except ValueError:
+                        print("Valor incorrecto")
+                        opcion_user = 0
+                        continue
             except ValueError:
                 print("Valor incorrecto")
+                continue
+
     #si la opcion es 5, sale del programa
     if opcion == 5:
-        break
-
-    else:
         break
 
     #aqui finaliza en programa y pregunta si desea repetir el programa
@@ -37,3 +72,4 @@ while exit != 1:
         exit = int(input("Desea salir del programa?: \n 1-Si \n 2-No \n=>"))
     except ValueError:
         print("Valor incorrecto")
+        continue
